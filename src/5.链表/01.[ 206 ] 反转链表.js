316ba49836ac71541b183d0f70634abc83975909
Ye Yang <1126439586@qq.com>
2021-01-12 18:29:28 +0800

    🚵‍♀️完成数组前二十题

--- src/5.链表/01.[ 206 ] 反转链表.js
+++ src/5.链表/01.[ 206 ] 反转链表.js
@@ -1,15 +1,37 @@
 /**
  * * 题目名称：反转链表
  * * 题目地址：https://leetcode-cn.com/problems/reverse-linked-list
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
  */
 
 // * 思路：
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
 
+};
 
 // 测试用例
 let test = ''
 
 console.time('执行用时');
-console.log(xxx(test));
+console.log(reverseList(test));
 console.timeEnd('执行用时');
